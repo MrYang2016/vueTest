a4663799708a542370dae6be0175b89a9e26d028
羊锡贵 <yangxigui@corp.polyv.net>
2024-04-08 09:04:08 +0800

    test 2

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+// 获取地址参数
+const urlParams = new URLSearchParams(window.location.search)
+console.log(urlParams.get('id')) // 获取地址栏参数id的值
+
 const app = createApp(App)
 
 app.use(createPinia())
